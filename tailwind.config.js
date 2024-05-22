122374ad68f3ac03ecc9506262e7b01c6166ea7d
Razan Muhammad Aysel <razanmaysel08@gmail.com>
2024-05-21 21:17:17 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Delimax': ['Delimax', 'sans-serif'],
+        'Bierg' : ['Bierg', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
